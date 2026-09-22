0fb85a0e3b99fd0c3e3021f1c3e756a44d1a36ca
Vladislav Svetlovskii <t1nky132@mail.ru>
2026-09-22 12:08:27 +0500

    test

--- internal/ui/convert_test.go
+++ internal/ui/convert_test.go
@@ -23,3 +23,21 @@ func TestHumanizeSizeInputSizeEqual1000(t *testing.T) {
 	result := humanizeSize(float64(1000))
 	assert.Equal(t, expected, result)
 }
+
+func TestUserPercentSuccess(t *testing.T) {
+	var expected float64 = 30
+	result := usedPercent(100, 70)
+	assert.Equal(t, expected, result)
+}
+
+func TestUserPercentTotalIsZero(t *testing.T) {
+	var expected float64 = 0
+	result := usedPercent(0, 70)
+	assert.Equal(t, expected, result)
+}
+
+func TestUserPercentAvailableIsZero(t *testing.T) {
+	var expected float64 = 100
+	result := usedPercent(100, 0)
+	assert.Equal(t, expected, result)
+}
